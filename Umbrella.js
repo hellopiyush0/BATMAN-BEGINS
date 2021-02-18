6aa679ea6e11e520c3038f47b98ce70c239fc36b
Piyush <74225753+hellopiyush0@users.noreply.github.com>
2021-02-18 21:59:21 +0530

    Add files via upload

---
+++ Umbrella.js
@@ -0,0 +1,34 @@
+class man{
+    constructor(x,y,r){
+      var options_man = {
+        isStatic: true,
+        restitution: 0.1,
+        friction: 0.5
+      }
+    
+    this.x = x;
+    this.y = y;
+    this.r = r;
+
+    this.image = loadImage("Walking Frame/walking_1.png");
+    
+
+    this.body = Bodies.circle(this.x,this.y,this.r,options_man);
+    World.add(world,this.body);
+
+    }
+
+    display(){
+        var pos = this.body.position;
+     push();
+     
+     translate(pos.x,pos.y);
+     rectMode(CENTER);
+     strokeWeight(2);
+     fill("silver");
+     imageMode(CENTER);
+     image(this.image,-15,40,420,420);
+
+     pop();
+    }
+}
